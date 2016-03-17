0db1cf9998137c0dd04146a527941a66c1f5d850
jiangyang-pd <jiangyang33@gmail.com>
2016-03-17 20:13:47 +0800

    add storage module

---
+++ cobweb/storage.py
@@ -0,0 +1,36 @@
+# 存储器
+import types
+import os
+
+
+class Storage:
+
+    def __init__(self, func=None):
+        self.base_dir = 'E:\Cobweb\cobweb\\'
+        if func is not None:
+            self.save = types.MethodType(func, self)
+
+    # 默认使用file储存
+    def save(self, filename, data):
+        path = self.base_dir + filename
+        # 如果文件存在则返回
+        if os.path.exists(path):
+            return False
+        try:
+            with open(path, 'w', encoding='utf8') as out:
+                print(str(data), file=out)
+        except IOError as err:
+            return False
+        return True
+
+
+def redis_save(self, key, value):
+    return True
+
+def mysql_save(self, value):
+    return True
+
+'''
+s = Storage(mysql_save)
+s.save('value')
+'''
